772d5d48f00e7afe38f08e901f9ebba3353c1061
Shayar Naveed <131683156+sharyarnaveed@users.noreply.github.com>
2025-03-15 16:38:19 +0500

    Add files via upload

--- lab6/src/main/java/com/example/demo/controller/BookingController.java
+++ lab6/src/main/java/com/example/demo/controller/BookingController.java
@@ -4,7 +4,9 @@ package com.example.demo.controller;
 import com.example.demo.model.BookingModel;
 import com.example.demo.services.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -42,5 +44,23 @@ public void deleteappointment(@PathVariable Long myId)
     Bservice.deletebyid(myId);
 }
 
+
+@PutMapping("/update/{myId}")
+    public BookingModel updateBook(@PathVariable Long myId, @RequestBody BookingModel NewEntry)
+{
+    BookingModel oldEntry=Bservice.findByid(myId).orElse(null);
+    if (oldEntry != null) {
+        oldEntry.setPatientName(NewEntry.getPatientName());
+        oldEntry.setDrName(NewEntry.getDrName());
+        oldEntry.setStatus(NewEntry.getStatus());
+
+        // Ensure the ID remains the same
+        oldEntry.setId(myId);
+
+        return Bservice.savedata(oldEntry);
+    } else {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Booking not found");
+    }
+}
 
 }
